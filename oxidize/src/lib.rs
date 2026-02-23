9d027a2dc92e111a8d17e8fe7d673d7d626eff6a
Cottons29 <lycuttons@gmail.com>
2026-02-24 00:17:07 +0700

    Re-export control and button

--- oxidize/src/lib.rs
+++ oxidize/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod widget {
     pub use widget::*;
     pub use constructor::Constructor;
-    pub use container::*; 
+    pub use container::*;
+    pub use control::*;
+    pub use control::gesture::button::*;
 }
 pub use engine::OxidizeApp;
 pub mod color {
