e8ad25298cf478baa7ced7c2e2ae73926744dd96
Cottons29 <lycuttons@gmail.com>
2026-03-02 02:33:02 +0700

    Add platform-specific entry points in `oxidize_main` macro for Android, WebAssembly, and desktop targets

--- macros/oxidize_main/src/lib.rs
+++ macros/oxidize_main/src/lib.rs
@@ -5,14 +5,34 @@ use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(item as ItemFn);
+    let fn_name = &input_fn.sig.ident;
     let block = &input_fn.block;
 
     let expanded = quote! {
-        // #input_fn
 
+        use oxidize::wasm_bindgen;
+        use oxidize::wasm_bindgen::prelude::wasm_bindgen;
+        #[inline]
+        #input_fn
+
+        #[cfg(not(target_arch = "wasm32"))]
         #[unsafe(no_mangle)]
-        pub extern "C" fn __oxidize_generated_entrance_point()
-            #block
+        pub extern "C" fn __oxidize_generated_entrance_point(){
+            #fn_name()
+        }
+
+        #[cfg(target_os = "android")]
+        #[unsafe(no_mangle)]
+        pub extern "C" fn android_main(app: oxidize::engine::winit::platform::android::activity::AndroidApp) {
+            let _ = oxidize::engine::oxidize::ANDROID_APP.set(app);
+            #fn_name()
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        #[wasm_bindgen]
+        pub fn __oxidize_generated_entrance_point(){
+            #fn_name()
+        }
         
     };
 
