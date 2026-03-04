701c1c920e7ce6ac8352309d8272c6bdc7ffe43e
Cottons29 <lycuttons@gmail.com>
2026-03-04 23:24:30 +0700

    Update `#oxidize_app` to use viewport-relative units (`vw`, `vh`) for consistent sizing

--- oxidize_cli/src/commands/create/web.rs
+++ oxidize_cli/src/commands/create/web.rs
@@ -56,15 +56,12 @@ export default defineConfig({
   </head>
   <style>
 
-    html, body {{
-      margin: 0;
-      padding: 0;
-      width: 100%;
-      height: 100%;
-    }}
     #oxidize_app {{
-      width: 100%;
-      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
       outline: none;
     }}
     </style>
