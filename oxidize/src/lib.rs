d7c7022febe5c28c6cf0188c27e2b9cc2fc019d7
Cottons29 <lycuttons@gmail.com>
2026-03-14 22:48:44 +0700

    Refactor imports in `oxidize.rs` for better module organization and ensure comprehensive re-export of widgets, utilities, and styles

--- oxidize/src/lib.rs
+++ oxidize/src/lib.rs
@@ -1,28 +1,61 @@
 // re-export all the widgets and utils
 
-pub mod widget {
-    pub use widget::*;
-    pub use constructor::Constructor;
-    pub use container::*;
-    pub use control::*;
-    pub use control::gesture::button::*;
-    pub use control::gesture::{AsyncCallback, Callback, CallbackHolder};
-}
-pub use engine::OxidizeApp;
-pub use engine;
-pub use oxidize_main::main;
 pub use animation::*;
+pub use attribute::dimension::Dimension;
+pub use attribute::position::Vec2d;
+pub use attribute::size::{ResolvedSize, Size};
+pub use color::prelude::*;
+pub use container::flex::row_column::*;
+pub use container::flex::*;
+pub use container::*;
+pub use control::gesture::button::{Button, ButtonStyle};
+pub use control::gesture::{AsyncCallback, Callback, CallbackHolder};
+pub use control::input::*;
+pub use control::*;
+pub use engine;
+pub use engine::OxidizeApp;
+pub use oxidize_main::main;
+pub use widget;
+pub use widget::Widget;
+pub use widget::base::BuildContext;
+pub use widget::text::Text;
+pub use widget::*;
+pub use widget::{State, StatefulWidget, StatelessWidget};
 
+// Macro re-export
+pub mod macros {
+    pub use constructor::Constructor;
+    pub use constructor::WidgetConstructor;
+    pub use oxidize_main::main;
+    pub use widget::widget_attr::widget;
+}
+
+// Styling re-export
+pub mod style {
+    pub use animation::AnimationEffect;
+    pub use animation::AnimationStatus;
+    pub use animation::curve::Curve;
+    pub use color::prelude::{Color, ColorMixer, Colors};
+    pub use container::flex::{BoxAlignment, LayoutDirection, OverflowBehavior};
+    pub use widget::style::BoxConstraint;
+    pub use widget::style::border::*;
+    pub use widget::style::layout_spacing::{LayoutSpacing, Spacing};
+    pub use widget::style::text_style::*;
+    pub use widget::text::{TextAlign, TextOverflow, TextStyle};
+}
+
+// utils re-export
 pub mod console {
     pub use utils::*;
 }
 
+// wasm dependencies
 pub mod wasm_bindgen {
     pub use wasm_bindgen::*;
 }
 
-pub mod color {
-    pub use color::prelude::*;
-}
-
 pub use router;
+pub use router::Navigator;
+pub use router::NavigatorController;
+pub use router::Route;
+pub use router::Router;
