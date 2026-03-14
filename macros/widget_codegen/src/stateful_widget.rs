a49a931ac8c486580b8cd2c18048ba549b4c409b
Cottons29 <lycuttons@gmail.com>
2026-03-14 22:48:12 +0700

    Refactor `StatefulWidget` trait implementation to simplify path usage and remove redundant namespace qualifiers

--- macros/widget_codegen/src/stateful_widget.rs
+++ macros/widget_codegen/src/stateful_widget.rs
@@ -14,9 +14,9 @@ pub fn generate_stateful_widget_impl(input: TokenStream) -> TokenStream {
     let output = quote! {
         #item_struct
 
-        impl #impl_generics oxidize::widget::Widget for #struct_name #ty_generics #where_clause {
-            fn to_element(&self, ctx: &oxidize::widget::base::BuildContext) -> Box<dyn oxidize::widget::Element> {
-                let (element, _updater) = oxidize::widget::StatefulElement::new(self, ctx);
+        impl #impl_generics widget::Widget for #struct_name #ty_generics #where_clause {
+            fn to_element(&self, ctx: &widget::base::BuildContext) -> Box<dyn widget::Element> {
+                let (element, _updater) = widget::StatefulElement::new(self, ctx);
                 Box::new(element)
             }
         }
