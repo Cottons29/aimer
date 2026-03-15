788b11a42f88f05e7902604cb3ff9d6636601f6c
Cottons29 <lycuttons@gmail.com>
2026-03-15 16:11:15 +0700

    Rename `Oxidize App` to `Aimer App` in `window.rs` for framework transition

--- aimer_cli/src/commands/create/window.rs
+++ aimer_cli/src/commands/create/window.rs
@@ -8,7 +8,7 @@ pub fn create(dir: &PathBuf) {
         r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
   <assemblyIdentity version="1.0.0.0" processorArchitecture="*" name="com.example.app" type="win32"/>
-  <description>Oxidize App</description>
+  <description>Aimer App</description>
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
     <security>
       <requestedPrivileges>
