b25f9abbbd472a4d202766146bdb2ee2b4198ddd
Cottons29 <lycuttons@gmail.com>
2026-03-18 23:05:43 +0700

    Refactor Android project file generation to use shared templates for improved maintainability

--- aimer_cli/src/commands/create/android.rs
+++ aimer_cli/src/commands/create/android.rs
@@ -10,8 +10,8 @@ pub fn create(dir: &PathBuf) {
     fs::create_dir_all(dir.join("builds/android/gradle/wrapper")).unwrap();
 
     let gradlew_path = dir.join("builds/android/gradlew");
-    fs::write(&gradlew_path, include_str!("assets/gradlew")).unwrap();
-    
+    fs::write(&gradlew_path, include_str!("../../../templates/android/dot_gradle/gradlew")).unwrap();
+
     #[cfg(unix)]
     {
         if let Ok(mut perms) = fs::metadata(&gradlew_path).map(|m| m.permissions()) {
@@ -20,33 +20,26 @@ pub fn create(dir: &PathBuf) {
         }
     }
 
-    fs::write(
-        dir.join("builds/android/gradlew.bat"),
-        include_str!("assets/gradlew.bat"),
-    )
-    .unwrap();
+    fs::write(dir.join("builds/android/gradlew.bat"), include_str!("../../../templates/android/dot_gradle/gradlew.bat")).unwrap();
 
     fs::write(
         dir.join("builds/android/gradle/wrapper/gradle-wrapper.properties"),
-        include_str!("assets/gradle-wrapper.properties"),
+        include_str!("../../../templates/android/dot_gradle/gradle-wrapper.properties"),
     )
     .unwrap();
 
     fs::write(
         dir.join("builds/android/gradle/wrapper/gradle-wrapper.jar"),
-        include_bytes!("assets/gradle-wrapper.jar"),
+        include_bytes!("../../../templates/android/dot_gradle/gradle-wrapper.jar"),
     )
     .unwrap();
 
-    fs::write(
-        dir.join("builds/android/gradle.properties"),
-        "android.useAndroidX=true\n",
-    )
-    .unwrap();
+    fs::write(dir.join("builds/android/gradle.properties"), "android.useAndroidX=true\n").unwrap();
 
     fs::write(
         dir.join("builds/android/build.gradle.kts"),
-        r#"plugins {
+        r#"
+plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
@@ -56,23 +49,7 @@ pub fn create(dir: &PathBuf) {
 
     fs::write(
         dir.join("builds/android/settings.gradle.kts"),
-        r#"pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "android"
-include(":app")
-"#,
+        include_str!("../../../templates/android/settings.gradle.kts.template"),
     )
     .unwrap();
 
@@ -81,72 +58,13 @@ include(":app")
 
     fs::write(
         dir.join("builds/android/app/build.gradle.kts"),
-        format!(r#"plugins {{
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}}
-
-android {{
-    namespace = "com.example.app"
-    compileSdk = 34
-
-    defaultConfig {{
-        applicationId = "com.example.app"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }}
-
-    buildTypes {{
-        release {{
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }}
-    }}
-    compileOptions {{
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }}
-    kotlinOptions {{
-        jvmTarget = "1.8"
-    }}
-}}
-
-dependencies {{
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-}}
-"#),
+        include_str!("../../../templates/android/build.gradle.kts.template"),
     )
     .unwrap();
 
     fs::write(
         dir.join("builds/android/app/src/main/AndroidManifest.xml"),
-        format!(r#"<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="com.example.app">
-
-    <application
-        android:allowBackup="true"
-        android:label="@string/app_name"
-        android:supportsRtl="true"
-        android:theme="@style/Theme.AppCompat.Light.NoActionBar">
-        <activity
-            android:name="android.app.NativeActivity"
-            android:exported="true"
-            android:configChanges="orientation|keyboardHidden|screenSize">
-            <meta-data android:name="android.app.lib_name" android:value="{0}" />
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
-                <category android:name="android.intent.category.LAUNCHER" />
-            </intent-filter>
-        </activity>
-    </application>
-
-</manifest>
-"#, lib_name),
+        include_str!("../../../templates/android/AndroidManifest.xml.template").replace("${app_name}", &lib_name),
     )
     .unwrap();
 
@@ -158,5 +76,4 @@ dependencies {{
 "#,
     )
     .unwrap();
-
 }
