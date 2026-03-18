f7182dc8f47b8e09e11c85a6af16306bc489de00
Cottons29 <lycuttons@gmail.com>
2026-03-19 00:41:07 +0700

    Refactor `Vec2d` to use `Float` type for unified platform-specific precision handling.

--- crates/attribute/src/position.rs
+++ crates/attribute/src/position.rs
@@ -1,28 +1,12 @@
+use crate::Float;
 use crate::size::ResolvedSize;
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Vec2d {
-    #[cfg(not(target_arch = "wasm32"))]
-    pub x: f32,
-    #[cfg(not(target_arch = "wasm32"))]
-    pub y: f32,
-    #[cfg(target_arch = "wasm32")]
-    pub x: f64,
-    #[cfg(target_arch = "wasm32")]
-    pub y: f64,
+    pub x: Float,
+    pub y: Float,
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-impl Vec2d {
-    pub fn get_end(&self, size: ResolvedSize) -> Vec2d {
-        Self {
-            x: self.x + size.width,
-            y: self.y + size.height,
-        }
-    }
-}
-
-#[cfg(target_arch = "wasm32")]
 impl Vec2d {
     pub fn get_end(&self, size: ResolvedSize) -> Vec2d {
         Self {
