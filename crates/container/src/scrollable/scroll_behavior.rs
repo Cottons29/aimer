2a6c738487fa0559da35c32d4093e7c5d4d35563
Cottons29 <lycuttons@gmail.com>
2026-03-29 22:40:08 +0700

    Refactor `ScrollBehavior`: replace `Float` with `f32` and remove unused import

--- crates/container/src/scrollable/scroll_behavior.rs
+++ crates/container/src/scrollable/scroll_behavior.rs
@@ -1,6 +1,5 @@
 use attribute::position::Vec2d;
 use constructor::Constructor;
-use crate::scrollable::Float;
 
 #[derive(Constructor)]
 pub struct ScrollBehavior {
@@ -11,11 +10,11 @@ pub struct ScrollBehavior {
     #[constructor(default = true)]
     pub bouncy: bool,
     #[constructor(default = 0.6)]
-    pub bouncy_resistance: Float,
+    pub bouncy_resistance: f32,
     #[constructor(default = 0.15)]
-    pub bouncy_recovery: Float,
+    pub bouncy_recovery: f32,
     #[constructor(default = 0.99)]
-    pub friction: Float,
+    pub friction: f32,
 }
 
 impl Default for ScrollBehavior {
@@ -26,7 +25,7 @@ impl Default for ScrollBehavior {
         let defaults = (0.6, 0.15, 0.98);
 
         Self {
-            max_scroll: Vec2d { x: Float::MAX, y: Float::MAX },
+            max_scroll: Vec2d { x: f32::MAX, y: f32::MAX },
             min_scroll: Vec2d { x: 0.0, y: 0.0 },
             velocity: Vec2d { x: 0.0, y: 0.0 },
             scroll_offset: Vec2d { x: 0.0, y: 0.0 },
