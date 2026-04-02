a7c292cc428b470ebb345b95af8bf9a637d59835
Cottons29 <lycuttons@gmail.com>
2026-04-02 14:38:28 +0700

    Refactor `callback.rs`: replace `CallbackHolder` with `VoidCallback` and introduce `CallbackExecutor` trait for streamlined callback handling

--- crates/control/src/callback.rs
+++ crates/control/src/callback.rs
@@ -1,109 +1,85 @@
+pub mod callback_inner;
+
+pub use callback_inner::*;
+use std::any::type_name;
 use std::cell::UnsafeCell;
-use std::pin::Pin;
+use std::fmt::Debug;
 use std::rc::Rc;
 
-/// A callback that can be either synchronous or asynchronous.
-// #[cfg(not(target_arch = "wasm32"))]
-pub enum Callback<P, R> {
-    Sync(Box<dyn Fn(P) -> R>),
-    Async(Box<dyn Fn(P) -> Pin<Box<dyn Future<Output = R> + Send>> + Send>),
+pub trait CallbackExecutor {
+    type Args;
+    type Output;
+    fn get(&self) -> &Option<RawInnerCallback<Self::Args, Self::Output>>;
+}
+pub struct Callback<Args = (), Return = ()> {
+    inner: CallbackInner<Args, Return>,
 }
 
-// #[cfg(target_arch = "wasm32")]
-// pub enum Callback<P, R> {
-//     Sync(Box<dyn Fn(P) -> R>),
-//     Async(Box<dyn Fn(P) -> Pin<Box<dyn Future<Output = ()>>>>),
-// }
 
-impl<P, R> std::fmt::Debug for Callback<P, R> {
+impl<Args, Return> Debug for Callback<Args, Return> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let p_type = std::any::type_name::<P>();
-        let r_type = std::any::type_name::<R>();
-        match self {
-            Callback::Sync(_) => write!(f, "Callback::Sync({p_type} -> {r_type})"),
-            Callback::Async(_) => write!(f, "Callback::Async({p_type} -> {r_type})"),
-        }
+        let args_type = type_name::<Args>();
+        let return_type = type_name::<Return>();
+        write!(f, "Callback({args_type} -> {return_type})",)
     }
 }
 
-// impl<P, R, F: Fn(P) -> R + 'static> From<F> for Callback<P, R> {
-//     fn from(f: F) -> Self {
-//         Callback::Sync(Box::new(f))
-//     }
-// }
-
-/// Wrapper to convert an async closure into a `Callback::Async`.
-pub struct AsyncCallback<F>(pub F);
-
-#[cfg(not(target_arch = "wasm32"))]
-impl<F, Fut, P, R> From<AsyncCallback<F>> for Callback<P, R>
-where
-    F: Fn(P) -> Fut + Send + 'static,
-    Fut: Future<Output = R> + Send + 'static,
-{
-    fn from(ac: AsyncCallback<F>) -> Self {
-        Callback::Async(Box::new(move |param| Box::pin(ac.0(param))))
-    }
-}
-
-// #[cfg(target_arch = "wasm32")]
-// impl<F, Fut> From<AsyncCallback<F>> for Callback
-// where
-//     F: Fn() -> Fut + 'static,
-//     Fut: Future<Output = ()> + 'static,
-// {
-//     fn from(ac: AsyncCallback<F>) -> Self {
-//         Callback::Async(Box::new(move || Box::pin(ac.0())))
-//     }
-// }
-
-/// A holder for a `Callback` that can be shared via `Rc`.
-/// Accepts both sync closures and `AsyncCallback`-wrapped async closures via `.into()`.
-#[derive(Debug)]
-pub struct CallbackHolder<P, R>(Rc<UnsafeCell<Option<Callback<P, R>>>>);
-
-impl<P, R> CallbackHolder<P, R> {
-    pub fn get(&self) -> *mut Option<Callback<P, R>> {
-        self.0.get()
-    }
-}
-
-impl<P, R> Default for CallbackHolder<P, R> {
-    fn default() -> Self {
-        CallbackHolder(Rc::new(UnsafeCell::new(None)))
-    }
-}
-
-impl<P, R> Clone for CallbackHolder<P, R> {
-    fn clone(&self) -> Self {
-        CallbackHolder(self.0.clone())
-    }
-}
-
-impl<P, R, F: Fn(P) -> R + 'static> From<F> for CallbackHolder<P, R> {
+impl<Args, Return, F: Fn(Args) -> Return + 'static> From<F> for Callback<Args, Return> {
     fn from(f: F) -> Self {
-        CallbackHolder(Rc::new(UnsafeCell::new(Some(Callback::Sync(Box::new(f))))))
+        Self { inner: CallbackInner::from(move |args| f(args)) }
     }
 }
 
-// #[cfg(not(target_arch = "wasm32"))]
-impl<P, R, F, Fut> From<AsyncCallback<F>> for CallbackHolder<P, R>
+impl<Args, Return, F, Fut> From<AsyncCallback<F>> for Callback<Args, Return>
 where
-    F: Fn(P) -> Fut + Send + 'static,
-    Fut: Future<Output = R> + Send + 'static,
+    F: Fn(Args) -> Fut + Send + 'static,
+    Fut: Future<Output = Return> + Send + 'static,
 {
     fn from(ac: AsyncCallback<F>) -> Self {
-        CallbackHolder(Rc::new(UnsafeCell::new(Some(Callback::Async(Box::new(move |param| Box::pin(ac.0(param))))))))
+        Self { inner: CallbackInner(Rc::new(UnsafeCell::new(Some(RawInnerCallback::Async(Box::new(move |args| Box::pin(ac.0(args)))))))) }
     }
 }
 
-// #[cfg(target_arch = "wasm32")]
-// impl<F, Fut> From<AsyncCallback<F>> for CallbackHolder
-// where
-//     F: Fn() -> Fut + 'static,
-//     Fut: Future<Output = ()> + 'static,
-// {
-//     fn from(ac: AsyncCallback<F>) -> Self {
-//         CallbackHolder(Rc::new(UnsafeCell::new(Some(Callback::Async(Box::new(move || Box::pin(ac.0())))))))
-//     }
-// }
+impl<P, R> CallbackExecutor for Callback<P,R> {
+    type Args = P;
+    type Output = R;
+    fn get(&self) -> &Option<RawInnerCallback<Self::Args, Self::Output>> {
+        unsafe { &*self.inner.get() }
+    }
+}
+
+
+#[derive(Default, Clone)]
+pub struct VoidCallback {
+    inner: CallbackInner<(), ()>,
+}
+
+impl Debug for VoidCallback {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("VoidCallback() -> ()").finish()
+    }
+}
+
+impl<F: Fn() + 'static> From<F> for VoidCallback {
+    fn from(f: F) -> Self {
+        Self { inner: CallbackInner::from(move |_| f()) }
+    }
+}
+
+impl<F, Fut> From<AsyncCallback<F>> for VoidCallback
+where
+    F: Fn() -> Fut + Send + 'static,
+    Fut: Future<Output = ()> + Send + 'static,
+{
+    fn from(ac: AsyncCallback<F>) -> Self {
+        Self { inner: CallbackInner(Rc::new(UnsafeCell::new(Some(RawInnerCallback::Async(Box::new(move |_| Box::pin(ac.0()))))))) }
+    }
+}
+
+impl CallbackExecutor for VoidCallback {
+    type Args = ();
+    type Output = ();
+     fn get(&self) -> &Option<RawInnerCallback<Self::Args, Self::Output>> {
+        unsafe { &*self.inner.get() }
+    }
+}
