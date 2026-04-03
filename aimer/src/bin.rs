0494fa8b9b8cc3ead9b42d11d084ce6f0bb30393
Cottons29 <lycuttons@gmail.com>
2026-04-03 17:58:44 +0700

    Simplify `main` function in `aimer_cli`: remove platform-specific conditionals

--- aimer/src/bin.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(any(target_os = "macos", target_os = "linux", target_os = "windows"))]
-fn main() {
-    aimer_cli::start_cli()
-}
-
-#[cfg(not(any(target_os = "macos", target_os = "linux", target_os = "windows")))]
-fn main() {}
