bec85395153bbfc30aa86ec1973a50b15febf343
Cottons29 <lycuttons@gmail.com>
2026-04-03 18:01:16 +0700

    Refactor `ios.rs`: integrate `cargo_build.rs`, streamline build logic, and modularize stdout/stderr handling

--- aimer_cli/src/commands/run/ios.rs
+++ aimer_cli/src/commands/run/ios.rs
@@ -1,16 +1,20 @@
 use crate::commands::run::Device;
+use crate::commands::run::cargo_build::{
+    self, CargoBuildTarget, stream_as_app_log_split_cr, stream_stderr_as_build_log, stream_stdout_with_xcode_progress, wait_for_child,
+};
 use crate::commands::run::console::{RunnerEvent, Status};
-use std::io::{BufRead, BufReader};
+use crossbeam::channel::Sender;
+use std::net::IpAddr;
 use std::process::{Child, Command, Stdio};
 use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
 
 pub fn spawn_ios_runner(
     device: Device,
     pkg_name: String,
-    tx: std::sync::mpsc::Sender<RunnerEvent>,
+    tx: Sender<RunnerEvent>,
     current_child_clone: Arc<Mutex<Option<Child>>>,
+    inspector_address: IpAddr,
+    inspector_port: u16,
 ) {
     let host_arch = match std::env::consts::ARCH {
         "aarch64" => "arm64",
@@ -24,74 +28,20 @@ pub fn spawn_ios_runner(
     let _ = tx.send(RunnerEvent::StatusChange(Status::Compiling(0)));
     let _ = tx.send(RunnerEvent::BuildLog(format!("Compiling static library for {}...", rust_target)));
 
-    let mut cargo_build = match Command::new("cargo")
-        .arg("build")
-        .arg("--lib")
-        .arg("--target")
-        .arg(rust_target)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn(){
-        Ok(build) => build,
-        Err(e) => {
-            let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to build static library: {}", e)));
-            let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
-            return;
-        }
-    };
-
-    let stdout = cargo_build.stdout.take().unwrap();
-    let stderr = cargo_build.stderr.take().unwrap();
-
-    *current_child_clone.lock().unwrap() = Some(cargo_build);
-
-    let tx_clone1 = tx.clone();
-    thread::spawn(move || {
-        let reader = BufReader::new(stdout);
-        for line in reader.lines() {
-            if let Ok(l) = line {
-                let _ = tx_clone1.send(RunnerEvent::BuildLog(l));
-            }
-        }
-    });
-
-    let tx_clone2 = tx.clone();
-    thread::spawn(move || {
-        let reader = BufReader::new(stderr);
-        let mut fetch_count = 0;
-        let mut compile_count = 0;
-        for line in reader.lines() {
-            if let Ok(l) = line {
-                if l.contains("Fetching") || l.contains("Updating") || l.contains("Downloading") {
-                    fetch_count = (fetch_count + 1).min(99);
-                    let _ = tx_clone2.send(RunnerEvent::StatusChange(Status::Fetching(fetch_count)));
-                } else if l.contains("Compiling") {
-                    compile_count = (compile_count + 1).min(99);
-                    let _ = tx_clone2.send(RunnerEvent::StatusChange(Status::Compiling(compile_count)));
-                } else if l.contains("Finished") {
-                    let _ = tx_clone2.send(RunnerEvent::StatusChange(Status::Compiling(100)));
-                }
-                let _ = tx_clone2.send(RunnerEvent::BuildLog(l));
-            }
-        }
-    });
-
-    let status = loop {
-        let mut guard = current_child_clone.lock().unwrap();
-        if let Some(child) = guard.as_mut() {
-            if let Ok(Some(status)) = child.try_wait() {
-                break status;
-            }
-        } else {
-            return;
-        }
-        drop(guard);
-        thread::sleep(Duration::from_millis(100));
+    let status = match cargo_build::spawn_cargo_build(
+        &CargoBuildTarget::Ios { rust_target: rust_target.to_string() },
+        &tx,
+        &current_child_clone,
+        inspector_address,
+        inspector_port,
+    ) {
+        Some(s) => s,
+        None => return,
     };
 
     if !status.success() {
         let _ = tx.send(RunnerEvent::BuildLog("Cargo build failed.".to_string()));
-        let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+        let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
         return;
     }
 
@@ -103,7 +53,7 @@ pub fn spawn_ios_runner(
     std::fs::create_dir_all(dest_dir).unwrap();
     if let Err(e) = std::fs::copy(&src_lib, &dest_lib) {
         let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to copy static library: {}", e)));
-        let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+        let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
         return;
     } else {
         let _ = tx.send(RunnerEvent::BuildLog(format!("Copied static library to {}", dest_lib)));
@@ -126,11 +76,12 @@ pub fn spawn_ios_runner(
         .current_dir("builds/ios")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .spawn(){
+        .spawn()
+    {
         Ok(build) => build,
         Err(e) => {
             let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to build Xcode project: {}", e)));
-            let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+            let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
             return;
         }
     };
@@ -140,49 +91,17 @@ pub fn spawn_ios_runner(
 
     *current_child_clone.lock().unwrap() = Some(xcode_build);
 
-    let tx_clone3 = tx.clone();
-    thread::spawn(move || {
-        let reader = BufReader::new(stdout);
-        let mut build_count = 0;
-        for line in reader.lines() {
-            if let Ok(l) = line {
-                if l.contains("Compile") || l.contains("Process") || l.contains("Link") {
-                    build_count = (build_count + 2).min(99);
-                    let _ = tx_clone3.send(RunnerEvent::StatusChange(Status::Building(build_count)));
-                } else if l.contains("** BUILD SUCCEEDED **") {
-                    let _ = tx_clone3.send(RunnerEvent::StatusChange(Status::Building(100)));
-                }
-                let _ = tx_clone3.send(RunnerEvent::BuildLog(l));
-            }
-        }
-    });
+    stream_stdout_with_xcode_progress(stdout, tx.clone());
+    stream_stderr_as_build_log(stderr, tx.clone());
 
-    let tx_clone4 = tx.clone();
-    thread::spawn(move || {
-        let reader = BufReader::new(stderr);
-        for line in reader.lines() {
-            if let Ok(l) = line {
-                let _ = tx_clone4.send(RunnerEvent::BuildLog(l));
-            }
-        }
-    });
-
-    let status = loop {
-        let mut guard = current_child_clone.lock().unwrap();
-        if let Some(child) = guard.as_mut() {
-            if let Ok(Some(status)) = child.try_wait() {
-                break status;
-            }
-        } else {
-            return;
-        }
-        drop(guard);
-        thread::sleep(Duration::from_millis(100));
+    let status = match wait_for_child(&current_child_clone) {
+        Some(s) => s,
+        None => return,
     };
 
     if !status.success() {
         let _ = tx.send(RunnerEvent::BuildLog("Xcodebuild failed.".to_string()));
-        let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+        let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
         return;
     }
 
@@ -201,14 +120,14 @@ pub fn spawn_ios_runner(
         Ok(status) => status,
         Err(e) => {
             let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to install on {}: {}", device_name, e)));
-            let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+            let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
             return;
         }
     };
 
     if !install_status.success() {
         let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to install on {}", device_name)));
-        let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+        let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
         return;
     }
 
@@ -220,11 +139,12 @@ pub fn spawn_ios_runner(
         .arg("CFBundleIdentifier")
         .arg("raw")
         .arg(&plist_path)
-        .output(){
+        .output()
+    {
         Ok(output) => output,
         Err(e) => {
             let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to get bundle id: {}", e)));
-            let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
+            let _ = tx.send(RunnerEvent::StatusChange(Status::Error));
             return;
         }
     };
@@ -234,21 +154,12 @@ pub fn spawn_ios_runner(
         .to_string();
 
     let mut app_run = match Command::new("xcrun")
-        .args([
-            "devicectl",
-            "device",
-            "process",
-            "launch",
-            "--terminate-existing",
-            "--console",
-            "--device",
-            &device.id,
-            &bundle_id,
-        ])
+        .args(["devicectl", "device", "process", "launch", "--terminate-existing", "--console", "--device", &device.id, &bundle_id])
         .env("TERM", "dumb")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .spawn(){
+        .spawn()
+    {
         Ok(run) => run,
         Err(e) => {
             let _ = tx.send(RunnerEvent::BuildLog(format!("Failed to launch app: {}", e)));
@@ -262,48 +173,12 @@ pub fn spawn_ios_runner(
 
     *current_child_clone.lock().unwrap() = Some(app_run);
 
-    let tx_clone5 = tx.clone();
-    thread::spawn(move || {
-        let reader = BufReader::new(stdout);
-        for line in reader.lines() {
-            if let Ok(l) = line {
-                for part in l.split('\r') {
-                    if !part.is_empty() {
-                        let _ = tx_clone5.send(RunnerEvent::AppLog(part.to_string()));
-                    }
-                }
-            }
-        }
-    });
-
-    let tx_clone6 = tx.clone();
-    thread::spawn(move || {
-        let reader = BufReader::new(stderr);
-        for line in reader.lines() {
-            if let Ok(l) = line {
-                for part in l.split('\r') {
-                    if !part.is_empty() {
-                        let _ = tx_clone6.send(RunnerEvent::AppLog(part.to_string()));
-                    }
-                }
-            }
-        }
-    });
+    stream_as_app_log_split_cr(stdout, tx.clone());
+    stream_as_app_log_split_cr(stderr, tx.clone());
 
     let _ = tx.send(RunnerEvent::StatusChange(Status::Running));
 
-    loop {
-        let mut guard = current_child_clone.lock().unwrap();
-        if let Some(child) = guard.as_mut() {
-            if let Ok(Some(_)) = child.try_wait() {
-                break;
-            }
-        } else {
-            return;
-        }
-        drop(guard);
-        thread::sleep(Duration::from_millis(100));
-    }
+    wait_for_child(&current_child_clone);
 
     let _ = tx.send(RunnerEvent::StatusChange(Status::Idling));
 }
