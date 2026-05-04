4cbd4b721cead06238134746f6b1cfb21b912774
Cottons29 <lycuttons@gmail.com>
2026-05-04 21:08:13 +0700

    refactor: rename `GestureDetectorElement` to `GestureDetector` and fix naming in layout cache, remove unused `to_css_color` method

--- crates/aimer_color/src/color.rs
+++ crates/aimer_color/src/color.rs
@@ -39,19 +39,6 @@ pub enum Color {
     Transparent,
 }
 
-impl Color {
-
-    fn to_css_color(&self) -> String {
-        let c = self.to_u32();
-        let a = ((c >> 24) & 0xFF) as f64 / 255.0;
-        let r = (c >> 16) & 0xFF;
-        let g = (c >> 8) & 0xFF;
-        let b = c & 0xFF;
-
-        format!("rgba({}, {}, {}, {})", r, g, b, a)
-    }
-}
-
 #[allow(clippy::derivable_impls)]
 impl Default for Color {
     fn default() -> Self {
