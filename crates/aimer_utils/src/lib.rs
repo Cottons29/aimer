e4585cd5e862198a3b674981d366e74d327b496c
Cottons29 <lycuttons@gmail.com>
2026-05-06 21:18:52 +0700

    feat: add `chrono` dependency and `time_consume` utility function for measuring execution time debug logs

--- crates/aimer_utils/src/lib.rs
+++ crates/aimer_utils/src/lib.rs
@@ -1,1 +1,10 @@
 pub mod log;
+
+
+pub fn time_consume<T>(label: &str, f: impl FnOnce() -> T) -> T {
+    let start = chrono::Local::now();
+    let res = f();
+    let delta = chrono::Local::now().signed_duration_since(start);
+    debug!("<{label}> time consume: {:?}", delta);
+    res
+}
