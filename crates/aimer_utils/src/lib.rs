8b5280b07d26a15a896ebd02563d8d0597ff9a47
Cottons29 <lycuttons@gmail.com>
2026-05-08 23:26:41 +0700

    refactor: add `ExecTimes` struct and refactor timing utility methods in `aimer_utils`

--- crates/aimer_utils/src/lib.rs
+++ crates/aimer_utils/src/lib.rs
@@ -1,10 +1,26 @@
 pub mod log;
 
+pub struct ExecTimes;
 
-pub fn time_consume<T>(label: &str, f: impl FnOnce() -> T) -> T {
-    let start = chrono::Local::now();
-    let res = f();
-    let delta = chrono::Local::now().signed_duration_since(start);
-    debug!("<{label}> time consume: {:?}", delta);
-    res
+impl ExecTimes {
+
+    pub fn no_param<T>(label: &str, f: impl FnOnce() -> T) -> T {
+        let start = chrono::Local::now();
+        let res = f();
+        let delta = chrono::Local::now().signed_duration_since(start);
+        debug!("<{label}> time consume: {:?}", delta);
+        res
+    }
+
+    pub fn with_param<T, P>(label: &str, param: P, f: impl FnOnce(P) -> T) -> T {
+        let start = chrono::Local::now();
+        let res = f(param);
+        let delta = chrono::Local::now().signed_duration_since(start);
+        debug!("<{label}> time consume: {:?}", delta);
+        res
+    }
+    
 }
+
+
+
