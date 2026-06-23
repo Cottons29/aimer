4181b0e59eb805c2b1de3c36dd39dc3228399cf5
Cottons29 <lycuttons@gmail.com>
2026-06-23 18:52:20 +0700

    feat(website): add web build scaffolding

---
+++ website/builds/web/main.ts
@@ -0,0 +1,8 @@
+import init, { __generated_entrance_point } from './pkg/website.js';
+// @ts-ignore
+async function main() {
+  await init();
+  __generated_entrance_point();
+}
+
+main().catch((_) => {});
